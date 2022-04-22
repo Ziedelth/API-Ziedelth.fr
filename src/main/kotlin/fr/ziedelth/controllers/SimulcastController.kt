1396fbc5440c138c92df027a1802f03bccaa5b56
Ziedelth <watteaupierre@gmail.com>
2022-04-22 17:08:10 +0200

    Add more cache and add anime scan in simulcast

--- src/main/kotlin/fr/ziedelth/controllers/SimulcastController.kt
+++ src/main/kotlin/fr/ziedelth/controllers/SimulcastController.kt
@@ -1,20 +1,7 @@
 package fr.ziedelth.controllers
 
-import fr.ziedelth.utils.ISO8601
-import fr.ziedelth.utils.Session
-import fr.ziedelth.utils.Simulcast
+import fr.ziedelth.caches.SimulcastCache
 
 class SimulcastController {
-    fun getSimulcasts(): List<Map<String, Any>>? {
-        val session = Session.sessionFactory.openSession()
-        val list =
-            session?.createQuery(
-                "SELECT DISTINCT releaseDate FROM Episode ORDER BY releaseDate ASC",
-                String::class.java
-            )
-                ?.list()?.toSet()
-        session?.close()
-        return list?.mapNotNull { Simulcast.getSimulcast(ISO8601.fromUTCDate(it)) }?.toSet()
-            ?.mapIndexed { index, simulcast -> mapOf("id" to index + 1, "simulcast" to simulcast) }
-    }
+    fun getSimulcasts(): List<Map<String, Any>>? = SimulcastCache.get()
 }
