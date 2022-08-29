779bf5c91e5d81399c5603c8819510c6dd6f6ac1
Ziedelth <watteaupierre@gmail.com>
2022-08-29 14:11:39 +0200

    Remove webp from optimizing

--- src/main/kotlin/fr/ziedelth/utils/Extensions.kt
+++ src/main/kotlin/fr/ziedelth/utils/Extensions.kt
@@ -5,3 +5,5 @@ import com.google.gson.Gson
 fun Any.toJSONString(): String = Gson().toJson(this).trim()
 fun Any.toBrotly(): String = Encode.brotli(this.toJSONString()).trim()
 fun Any.toGZIP(): String = Encode.gzip(this.toJSONString()).trim()
+
+fun Double.toString(number: Int) = String.format("%.${number}f", this)
